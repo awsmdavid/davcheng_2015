08930f87aab69f7c9ccc6ecb6eff876a4cd2aba4
awsmdavid <awsmdavid@gmail.com>
2015-01-21 23:52:36 -0500

    bg js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+	var bg_img = ['bg-sunrise', 'bg-hood', 'bg-chelsea-pier', 'bg-portland', 'bg-lucerne', 'bg-jap-garden', 'bg-london-bridge'];
+	$("body").animate({ opacity : "1.0" }, 500);
+	var x=1;
+    $('#home-icon').click(function(){
 
-    $("body").animate({ opacity : "1.0" }, 500);
-
+		$('#background-image').removeClass();
+		$('#background-image').addClass(bg_img[x]);
+		if(x<6){
+			x+=1;
+		}
+		else{
+			x=0;
+		}
+    });
 });
