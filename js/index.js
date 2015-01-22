9274cce14d6817558b3707e79cae73da90049153
awsmdavid <awsmdavid@gmail.com>
2015-01-22 00:11:21 -0500

    bg updates

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	var bg_img = ['bg-sunrise', 'bg-hood', 'bg-chelsea-pier', 'bg-portland', 'bg-lucerne', 'bg-jap-garden', 'bg-london-bridge'];
+	var bg_img = ['bg-sunrise', 'bg-hood', 'bg-chelsea-pier', 'bg-portland', 'bg-lucerne', 'bg-nats', 'bg-jap-garden', 'bg-london-bridge'];
 	$("body").animate({ opacity : "1.0" }, 500);
 	var x=1;
     $('#home-icon').click(function(){
 
 		$('#background-image').removeClass();
 		$('#background-image').addClass(bg_img[x]);
-		if(x<6){
+		if(x<bg_img.length-1){
 			x+=1;
 		}
 		else{
